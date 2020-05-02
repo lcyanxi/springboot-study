c24ab4001f03176c78a5f0e116f6d4d1101665e6
lichang <lichang@koolearn.com>
2020-05-02 16:39:41 +0800

    添加配置修改

--- springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
+++ springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
@@ -1,12 +1,8 @@
 package com.lcyanxi;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDubbo
